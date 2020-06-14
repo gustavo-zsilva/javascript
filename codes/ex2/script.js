cacc72b934ebd27560f72af91731b268ca5e1b07
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-06-14 19:17:01 -0300

    ex2

--- codes/ex2/script.js
+++ codes/ex2/script.js
@@ -1,1 +1,9 @@
-alert('Hello World!')
+var lampada = document.querySelector('img')
+
+function ligar() {
+    lampada.setAttribute('src', '../ex2/images/lampada-acesa.png')
+}
+
+function desligar() {
+    lampada.setAttribute('src', '../ex2/images/lampada-apagada.png')
+}
