2d5ed8f11470afa168b4e107a54f272b9bd2dcef
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-06-14 19:25:57 -0300

    ex2.1

--- codes/ex2/script.js
+++ codes/ex2/script.js
@@ -1,9 +1,18 @@
 var lampada = document.querySelector('img')
 
-function ligar() {
-    lampada.setAttribute('src', '../ex2/images/lampada-acesa.png')
-}
+function funcoes(id) {
+    switch (id >= 0 && id < 4) {
+        case id == 0:
+            lampada.setAttribute('src', '../ex2/images/lampada-acesa.png')
+            break
+        case id == 1:
+            lampada.setAttribute('src', '../ex2/images/lampada-apagada.png')
+            break
+        case id == 2:
+            lampada.style.display = 'none'
+            break
+        default:
+            lampada.style.display = 'flex'
 
-function desligar() {
-    lampada.setAttribute('src', '../ex2/images/lampada-apagada.png')
+    }
 }
